643505a6318640f19a95735eb254e3eb034cd2f5
Vahe Sevachyan <vscode13@gmail.com>
2024-01-15 18:48:16 -0800

    starting project manager app

---
+++ src/components/HeaderMain.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <h1>Project Manager</h1>
+  </header>
+</template>
+<script>
+export default {
+  name: "HeaderMain",
+  props: {
+    title: String,
+  },
+};
+</script>
+<style>
+header {
+  display: flex;
+}
+</style>

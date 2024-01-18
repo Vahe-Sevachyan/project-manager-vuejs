b8e22ec8429701cba0174b713f87bf1c2c6f57e5
Vahe Sevachyan <vscode13@gmail.com>
2024-01-17 21:29:55 -0800

    added TasksList component

--- src/components/SetTasks.vue
+++ src/components/SetTasks.vue
@@ -1,13 +1,18 @@
 <template>
   <div v-bind:key="task.id" v-for="task in SetTasks">
     <h3>{{ task.text }}</h3>
+    <TasksList />
   </div>
 </template>
 <script>
+import TasksList from "./TasksList";
 export default {
   name: "SetTasks",
   props: {
     tasks: Array,
+  },
+  components: {
+    TasksList,
   },
 };
 </script>
---
+++ src/components/TasksList.vue
@@ -0,0 +1,12 @@
+<template>
+  <h3>{{ TasksList.text }}</h3>
+</template>
+
+<script>
+export default {
+  name: "TasksList",
+  props: {
+    task: Object,
+  },
+};
+</script>

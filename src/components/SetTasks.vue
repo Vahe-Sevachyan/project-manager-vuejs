b1f7980b48541f97f833bca93bf81939adb44e10
Vahe Sevachyan <vscode13@gmail.com>
2024-01-17 21:42:02 -0800

    added styles to setTasks component

--- src/components/SetTasks.vue
+++ src/components/SetTasks.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-bind:key="task.id" v-for="task in SetTasks">
     <h3>{{ task.text }}</h3>
-    <TasksList />
+    <TasksList :task="task" />
   </div>
 </template>
 <script>
@@ -16,3 +16,19 @@ export default {
   },
 };
 </script>
+<style>
+.task {
+  background: 5px;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.runner {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>

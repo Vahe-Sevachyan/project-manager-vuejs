ee170a06dd0240b31ef39836ba8e103887ddc1c0
Vahe Sevachyan <vscode13@gmail.com>
2024-01-17 21:44:28 -0800

    updated styles to correct component

--- src/components/SetTasks.vue
+++ src/components/SetTasks.vue
@@ -16,19 +16,3 @@ export default {
   },
 };
 </script>
-<style>
-.task {
-  background: 5px;
-  margin: 5px;
-  padding: 10px 20px;
-  cursor: pointer;
-}
-.task.runner {
-  border-left: 5px solid green;
-}
-.task h3 {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-</style>
